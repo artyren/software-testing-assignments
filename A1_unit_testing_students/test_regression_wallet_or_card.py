31a1c4bc60acdec38c6221406c0f96635d492e1e
Sofia Alfsson <sofiaalfssons@gmail.com>
2023-12-18 15:41:51 +0100

    stub tests added to new card implementation

--- A1_unit_testing_students/test_regression_wallet_or_card.py
+++ A1_unit_testing_students/test_regression_wallet_or_card.py
@@ -5,3 +5,11 @@ import shutil
 import os
 from unittest.mock import * 
 
+def test_pay_with_wallet_card_implemented():
+    return 0
+
+def test_pay_with_card():
+    return 0
+
+def test_pay_with_card_several_cards():
+    return 0
